a989072e34ec4382e177df65d9d1aab43d04bc20
Lucas Erickson <148932045+Luked-E@users.noreply.github.com>
2024-09-24 15:49:48 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Tri-Rubix
+ A console tool to explore the basic tri-pyramid rubix cube.
---
+++ TriRubix/TriRubix/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace TriRubix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        class TriCube
+        {
+            byte[,] topTurn =
+            {
+                {1,2 }, {2,3 }, {3,4 }, {4,1 },
+            };
+            byte[,] frontTurn =
+            {
+                {2,1 }, {1,5 }, {5,6 }, {6,2 },
+            };
+            byte[,] rightTurn =
+            {
+                { }, { }, { }, { },
+            };
+        }
+        class TriCubelet
+        {
+            int posX = 0; int posY = 0; int posZ = 0;
+            int rotX = 0; int rotY = 0; int rotZ = 0;
+        }
+    }
+}
